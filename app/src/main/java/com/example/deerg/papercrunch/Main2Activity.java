1484494b0a8487510498ab3545d5efaa0902e893
deergh <deerghsingh11@gmail.com>
2019-06-12 15:20:45 +0530

    teat

---
+++ app/src/main/java/com/example/deerg/papercrunch/Main2Activity.java
@@ -0,0 +1,133 @@
+package com.example.deerg.papercrunch;
+
+import android.content.Intent;
+import android.database.DataSetObserver;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Main2Activity extends AppCompatActivity {
+
+    android.support.v7.widget.Toolbar custom_toolbar;
+    DrawerLayout mDrawerLayout;
+    ActionBarDrawerToggle mActionBarDrawerToggle;
+    NavigationView navigationView;
+    ExpandableListView mExpandableListView;
+    ExpandableListAdapter mExpandableListAdapter;
+    List<String> listheader;
+    HashMap<String, List<String>> listchild;
+    private ExpandableListView listView;
+    private aExpandableListAdapter listAdapter;
+    private List<String> listDataHeader;
+    private HashMap<String,List<String>> listHash;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        setuptoolbar();
+        Toast.makeText(getApplicationContext(),"Child Clicked",Toast.LENGTH_SHORT);
+        listView = (ExpandableListView)findViewById(R.id.lvExp);
+        initData();
+        listAdapter = new com.example.deerg.papercrunch.aExpandableListAdapter(this,listDataHeader,listHash);
+        listView.setAdapter(listAdapter);
+        listView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+                Intent intent=new Intent(getApplicationContext(),ConceptScreen.class);
+                startActivity(intent);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.onerflow_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setuptoolbar() {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            custom_toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.custom_toolbar);
+        }
+        setSupportActionBar(custom_toolbar);
+        mExpandableListView = (ExpandableListView) findViewById(R.id.navmenu);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        prepareData();
+        mExpandableListAdapter = new com.example.deerg.papercrunch.ExpandableListAdapter(this, listheader, listchild, mExpandableListView);
+        mExpandableListView.setAdapter(mExpandableListAdapter);
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, custom_toolbar, R.string.app_name, R.string.app_name);
+        mDrawerLayout.addDrawerListener(mActionBarDrawerToggle);
+        mActionBarDrawerToggle.syncState();
+        navigationView.setItemIconTintList(null);
+    }
+
+    private void prepareData() {
+        listheader = new ArrayList<String>();
+        listchild = new HashMap<String, List<String>>();
+        listheader.add("View All Sub Levels");
+        listheader.add("View Prevoius Level");
+        listheader.add("View Next Level");
+        listheader.add("Rate us");
+        listheader.add("About us");
+
+        List<String> head1 = new ArrayList<String>();
+        head1.add("Sub level 1");
+        head1.add("Sub level 2");
+        head1.add("Sub level 3");
+        head1.add("Sub level 4");
+
+        List<String> head2 = new ArrayList<String>();
+        head2.add("Level 1");
+        head2.add("Level 2");
+        head2.add("Level 3");
+        head2.add("Level 4");
+
+        listchild.put(listheader.get(0), head1);
+        listchild.put(listheader.get(1), head2);
+    }
+    private void initData(){
+        listDataHeader = new ArrayList<>();
+        listHash = new HashMap<>();
+
+        listDataHeader.add("Levlel");
+        listDataHeader.add("asd");
+
+        List<String> lleev = new ArrayList<>();
+        lleev.add("Expanded");
+
+        List<String> lev2 = new ArrayList<>();
+        lev2.add("sub");
+        lev2.add("suub");
+
+        listHash.put(listDataHeader.get(0),lleev);
+        listHash.put(listDataHeader.get(1),lev2);
+
+    }
+}
---
+++ app/src/main/java/com/example/deerg/papercrunch/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.deerg.papercrunch;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar pg;
+    Handler mHandler=new Handler();
+    private int mprogressbar=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        pg=(ProgressBar)findViewById(R.id.progressBar);
+        final Intent i = new Intent(this,login.class);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(mprogressbar<100)
+                {
+                    mprogressbar++;
+                    android.os.SystemClock.sleep(30);
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            pg.setProgress(mprogressbar);
+                        }
+                    });
+                }
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(i);
+                    }
+                });
+                finish();
+            }
+        }).start();
+
+    }
+
+}
